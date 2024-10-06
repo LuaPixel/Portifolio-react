7797d6a5ce8ca97d8e0126b350081b08cd313793
Luana <luanabmarcolino9c@gmail.com>
2024-10-06 20:45:07 -0300

    alterações

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,11 @@ import Linkedin from "./asstes/linke.jpg";
 
 import Github from "./asstes/github.jpg";
 
-import Progr from "./asstes/prog.jpg";
+
 
 import ilus from "./asstes/ilus.jpg";
 
-export default function Main() {
+export default function Main(){
 
     return(
     <main className="d-flex bg-black w-100 min-vh-100 mx-auto">
@@ -20,7 +20,7 @@ export default function Main() {
             <div className="icons d-flex flex-row justify-content-center mb-4 ">
                 <a href="https://br.linkedin.com/?src=go-pa&trk=sem-ga_campid.12619604099_asid.149519181115_crid.657343811713_kw.linkedin_d.c_tid.kwd-148086543_n.g_mt.e_geo.9197208&mcid=6821526239111716925&cid=&gad_source=1&gclid=Cj0KCQjwpP63BhDYARIsAOQkATaYA5fxlh1EL69Lz_zKLFktA7pns66F95I68LJ59vcpn0CTtexRsikaAj_nEALw_wcB&gclsrc=aw.ds&original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank" rel="noopener noreferrer"  className="hreff d-flex"> 
 
-                <img src={Linkedin} className="linke " / >
+                <img src={Linkedin} className="linke " />
                 </a>
                 
                 <a href="https://github.com/LuaPixel" target="_blank" rel="noopener noreferrer" className="hreff d-flex">
@@ -30,7 +30,7 @@ export default function Main() {
                 
             </div>
 
-            <button onClick={() => window.open("https://wa.me/qr/347MNYFBV74TF1")} alt="Contato" className="work " target="_blank" text="Work toghether">Transforme Suas Ideias em Realidade <br/>   — Fale Comigo</button>
+            <button onClick={() => window.open("https://wa.me/qr/347MNYFBV74TF1")} alt="Contato" className="work " target="_blank" text="Work toghether"> Transforme Suas Ideias em Realidade <br/>   — Fale Comigo</button>
 
             
             
